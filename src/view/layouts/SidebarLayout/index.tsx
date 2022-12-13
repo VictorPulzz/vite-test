e579efe3cb7b833460238f109c5eb708498f6e90
Roman Stepura <RStepura44@gmail.com>
2022-12-13 11:07:57 +0200

    add fonts

--- src/view/layouts/SidebarLayout/index.tsx
+++ src/view/layouts/SidebarLayout/index.tsx
@@ -37,20 +37,20 @@ export const SidebarLayout: FC<Props> = ({ children, contentClassName }) => {
       <div className={styles['sidebar']}>
         <div className="flex justify-between px-5">
           <Link to={ROUTES.HOME}>
-            <img src={logo} alt="Logo" className="w-[4.5rem] h-[1.3rem]" />
+            <img src={logo} alt="Logo" className="w-[4.9rem] h-[1.6rem]" />
           </Link>
         </div>
         <nav className="mt-6">
           <ul>
             {navItems.map(item => (
-              <li key={item.link} className="p-3">
+              <li key={item.link} className="px-3 py-2">
                 <NavLink
                   to={item.link}
                   className={({ isActive }) =>
                     clsx(styles['nav__link'], { [styles['nav__link--active']]: isActive })
                   }
                 >
-                  <Icon name={item.icon} size="1.1rem" className="mr-3" />
+                  <Icon name={item.icon} size="1.125rem" className="mr-3" />
                   {item.title}
                 </NavLink>
               </li>
