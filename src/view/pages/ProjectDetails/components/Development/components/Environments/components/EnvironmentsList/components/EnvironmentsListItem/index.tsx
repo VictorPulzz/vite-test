40254c584d95ca22f3fefee60aea2d80c6e87a45
Roman Stepura <RStepura44@gmail.com>
2023-03-09 12:56:14 +0200

    SB2_066: Develop request card

--- src/view/pages/ProjectDetails/components/Development/components/Environments/components/EnvironmentsList/components/EnvironmentsListItem/index.tsx
+++ src/view/pages/ProjectDetails/components/Development/components/Environments/components/EnvironmentsList/components/EnvironmentsListItem/index.tsx
@@ -3,6 +3,8 @@ import React, { FC, useMemo } from 'react';
 
 import { ProjectEnvironmentType } from '~/services/gql/__generated__/globalTypes';
 import { convertUppercaseToReadable } from '~/utils/convertUppercaseToReadable';
+import { RequestCard } from '~/view/pages/ProjectDetails/components/Development/components/RequestCard';
+import { CardVariant } from '~/view/pages/ProjectDetails/consts';
 import { Icon } from '~/view/ui/components/common/Icon';
 import { IconContainer } from '~/view/ui/components/common/IconContainer';
 
@@ -10,10 +12,12 @@ import { EnvironmentsListItemMenu } from './components/EnvironmentsListItemMenu'
 
 interface Props {
   environment: ProjectEnvironmentType;
+  variant?: CardVariant;
 }
 
 export const EnvironmentsListItem: FC<Props> = ({
   environment: { name, backendCredentials, frontendCredentials },
+  variant = CardVariant.DEFAULT,
 }) => {
   const backendCredentialsData = useMemo(
     () => (backendCredentials ? pick(backendCredentials, ['url', 'login', 'password']) : null),
@@ -26,41 +30,46 @@ export const EnvironmentsListItem: FC<Props> = ({
   );
 
   return (
-    <div className="p-5 border-solid border border-gray-5 rounded-xl">
-      <div className="flex items-center justify-between border-solid border-b border-gray-5 pb-3">
-        <div className="flex items-center gap-2">
-          <IconContainer name="code" className="w-10 h-10 bg-blue/10" iconClassName="w-5 h-5" />
-          <div>
-            <h2 className="text-p4 font-medium">{convertUppercaseToReadable(name)}</h2>
-            <span className="text-c1 text-gray-1">Credentials</span>
-          </div>
-        </div>
-        <EnvironmentsListItemMenu />
-      </div>
-      <div className="mt-3 flex items-center justify-between">
-        <div>
-          <span className="text-c1 text-gray-1">Backend</span>
-          <div className="mt-2 flex flex-col gap-3">
-            {Object.entries(backendCredentialsData || {})?.map(([key, value]) => (
-              <div key={key} className="flex items-center gap-1">
-                <Icon name={key} size={18} color="#6F6F75" />
-                <span className="text-c1">{value}</span>
+    <>
+      {variant === CardVariant.DEFAULT && (
+        <div className="p-5 border-solid border border-gray-5 rounded-xl">
+          <div className="flex items-center justify-between border-solid border-b border-gray-5 pb-3">
+            <div className="flex items-center gap-2">
+              <IconContainer name="code" className="w-10 h-10 bg-blue/10" iconClassName="w-5 h-5" />
+              <div>
+                <h2 className="text-p4 font-medium">{convertUppercaseToReadable(name)}</h2>
+                <span className="text-c1 text-gray-1">Credentials</span>
               </div>
-            ))}
+            </div>
+            <EnvironmentsListItemMenu />
           </div>
-        </div>
-        <div>
-          <span className="text-c1 text-gray-1">Frontend</span>
-          <div className="mt-2 flex flex-col gap-3">
-            {Object.entries(frontendCredentialsData || {})?.map(([key, value]) => (
-              <div key={key} className="flex items-center gap-1">
-                <Icon name={key} size={18} color="#6F6F75" />
-                <span className="text-c1">{value}</span>
+          <div className="mt-3 flex items-center justify-between">
+            <div>
+              <span className="text-c1 text-gray-1">Backend</span>
+              <div className="mt-2 flex flex-col gap-3">
+                {Object.entries(backendCredentialsData || {})?.map(([key, value]) => (
+                  <div key={key} className="flex items-center gap-1">
+                    <Icon name={key} size={18} color="#6F6F75" />
+                    <span className="text-c1">{value}</span>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+            <div>
+              <span className="text-c1 text-gray-1">Frontend</span>
+              <div className="mt-2 flex flex-col gap-3">
+                {Object.entries(frontendCredentialsData || {})?.map(([key, value]) => (
+                  <div key={key} className="flex items-center gap-1">
+                    <Icon name={key} size={18} color="#6F6F75" />
+                    <span className="text-c1">{value}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+      {variant === CardVariant.REQUEST && <RequestCard title={name} icon="code" />}
+    </>
   );
 };
