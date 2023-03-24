3f75455e189f47a01108a67057d7e2cca277703e
gmakarov <german@appello.com.au>
2023-03-24 12:52:11 +0400

    fix bundle issues

--- webpack-config/webpack.dev.js
+++ webpack-config/webpack.dev.js
@@ -8,6 +8,7 @@ const devServer = require('./dev-server');
  *
  * @type {{output: {chunkFilename: string, path: *, filename: string, publicPath: string}, mode: (string|false), entry: string, resolve: {extensions: string[], mainFields: string[], alias: {"~root": string, "~": string}}, plugins: (CopyPlugin|false|ReactRefreshPlugin|*|MiniCssExtractPlugin|CleanWebpackPlugin)[], module: {rules: [{test: RegExp, use: {loader: string, options: {getCustomTransformers: (function(): {before}), transpileOnly: boolean}}[]},{test: *, use: (string|{loader: string, options})[], exclude: *, sideEffects: boolean},{test: *, use: (string|{loader: string, options})[]},{test: *, use: (string|{loader: string, options})[], exclude: *, sideEffects: boolean},{test: *, use: (string|{loader: string, options})[]},null,null]}, context: string, target: string[]}}
  */
+
 module.exports = merge(common(DEV_MODE), {
   mode: 'development',
   devServer,
