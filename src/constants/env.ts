c9398e1cdf33b5b81ece34acde706d82c849d4c3
gmakarov <german@appello.com.au>
2023-03-29 13:08:09 +0400

    feat(vite): apply vite config

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,4 @@
 // dotenv-webpack plugin throws error if env variables are empty
-export const API_URL = process.env.API_URL as string;
+export const API_URL = import.meta.env.VITE_API_URL as string;
 
 export const REFRESH_TOKEN_URL = '/auth/token/refresh';
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { checker } from 'vite-plugin-checker';
+import { svgSpritemap } from 'vite-plugin-svg-spritemap';
+
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+        '@root': path.resolve(__dirname),
+        '@ui': path.resolve(__dirname, './src/view/ui'),
+      },
+    },
+    envDir: './env',
+    build: {
+      outDir: 'build',
+    },
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+      }),
+      svgSpritemap({
+        pattern: 'src/view/assets/icons/*.svg',
+        filename: 'icons/spritemap.svg',
+      }),
+      svgSpritemap({
+        pattern: 'src/view/assets/icons/raw/*.svg',
+        filename: 'icons/raw-spritemap.svg',
+      }),
+    ],
+  };
+});
