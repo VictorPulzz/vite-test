44f443996a92eef46a254fbbcce78103bdae722c
German Makarov <german@appello.com.au>
2023-04-27 17:52:00 +0000

    Merged in SB2_112/bugfix (pull request #128)
    fix(SB2_112): log out user when resetting password
    
    * fix(SB2_112): log out user when resetting password

--- src/view/pages/ResetPassword/index.tsx
+++ src/view/pages/ResetPassword/index.tsx
@@ -5,6 +5,8 @@ import React, { FC } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { ROUTES } from '~/constants/routes';
+import { useAppDispatch } from '~/store/hooks';
+import { signOut } from '~/store/modules/user';
 import { AuthLayout } from '~/view/layouts/AuthLayout';
 
 import { useResetPasswordForm } from './hooks/useResetPasswordForm';
@@ -13,10 +15,14 @@ export const ResetPasswordPage: FC = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const token = searchParams.get('key') ?? '';
+  const dispatch = useAppDispatch();
 
   const { form, handleSubmit } = useResetPasswordForm({
     token,
-    onSubmitSuccessful: () => navigate(ROUTES.SIGN_IN),
+    onSubmitSuccessful: () => {
+      dispatch(signOut());
+      navigate(ROUTES.SIGN_IN);
+    },
   });
 
   return (
