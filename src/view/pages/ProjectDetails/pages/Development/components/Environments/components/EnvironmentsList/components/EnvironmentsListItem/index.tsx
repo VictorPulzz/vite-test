d50d6adfb52609e99972b08dbc8b310bf056e1d4
rstepura44 <rstepura44@gmail.com>
2023-07-06 09:52:12 +0000

    Merged in SB2_224/hide-context-menu-in-project-details-development (pull request #195)
    SB2_224: hide context menu if user doesn't have permission write project env/integrations
    
    * SB2_224: hide context menu if user doesn't have permission write project env/integrations

--- src/view/pages/ProjectDetails/pages/Development/components/Environments/components/EnvironmentsList/components/EnvironmentsListItem/index.tsx
+++ src/view/pages/ProjectDetails/pages/Development/components/Environments/components/EnvironmentsList/components/EnvironmentsListItem/index.tsx
@@ -2,8 +2,10 @@ import { pick } from '@appello/common/lib/utils/object/pick';
 import { IconContainer } from '@appello/web-ui';
 import React, { FC, useMemo } from 'react';
 
+import { Permission } from '~/constants/permissions';
 import { ProjectEnvironmentType } from '~/services/gql/__generated__/globalTypes';
 import { convertUppercaseToReadable } from '~/utils/convertUppercaseToReadable';
+import { useHasAccess } from '~/view/hooks/useHasAccess';
 import { CardVariant } from '~/view/pages/ProjectDetails/consts';
 import { RequestCard } from '~/view/pages/ProjectDetails/pages/Development/components/RequestCard';
 
@@ -23,6 +25,8 @@ export enum EnvironmentCredentialsType {
 }
 
 export const EnvironmentsListItem: FC<Props> = ({ environment, envRequest, variant }) => {
+  const canWriteProjectEnvs = useHasAccess(Permission.WRITE_PROJECT_ENVS);
+
   const frontendCredentialsData = useMemo(
     () =>
       environment?.frontendCredentials
@@ -53,7 +57,9 @@ export const EnvironmentsListItem: FC<Props> = ({ environment, envRequest, varia
                 <span className="text-p5 text-gray-1">Credentials</span>
               </div>
             </div>
-            <EnvironmentsListItemMenu id={environment.id} name={environment.name} />
+            {canWriteProjectEnvs && (
+              <EnvironmentsListItemMenu id={environment.id} name={environment.name} />
+            )}
           </div>
           <div className="mt-3 grid grid-cols-2 justify-between gap-10">
             <EnvironmentСredentials label="Frontend" data={frontendCredentialsData} />
